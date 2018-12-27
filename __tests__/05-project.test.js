4624ba6650c8c2e017c128dd856e476467f40819
jacob <1anderson2jacob@gmail.com>
2018-12-27 08:16:15 -0800

    tests complete, need tested

--- __tests__/05-project.test.js
+++ __tests__/05-project.test.js
@@ -1,33 +1,60 @@
 'use strict';
 
-// const darken = require('../transforms/darken.js');
-// const johndice = require('../transforms/johndice.js');
-// const random = require('../transforms/random.js');
+const darken = require('../transforms/darken.js');
+const johndice = require('../transforms/johndice.js');
+const random = require('../transforms/random.js');
 const redscale = require('../transforms/redscale.js');
 
+const getRandomNum = () => {
+  return Math.floor(Math.random() * Math.floor(256));
+  
+};
 
-let testColorArray = [];
+let bmp = {
+  testColorArray: [],
+};
 
 for (let i=0; i < 100; i++) {
-  testColorArray.push(0);
+  bmp.colorArray.push(9);
 }
 
-// let darkenColorArray = darken(testColorArray);
-// let johndiceColorArray = johndice(testColorArray);
-// let randomColorArray = random(testColorArray);
-let redScaleColorArray = redscale(testColorArray);
+let darkenColorArray = darken(bmp.colorArray);
+let johndiceColorArray = johndice(bmp.colorArray);
+let redScaleColorArray = redscale(bmp.colorArray);
 
 describe('transform modules', () => {
   describe('darken', () => {
+    it('should darken the image by dividing each rgb value by 3', () => {
+      let expectedB = bmp.colorArray[0]/3;
+      let expectedG = bmp.colorArray[1]/3;
+      let expectedR = bmp.colorArray[2]/3;
 
+      let darkIndexB = darkenColorArray[0];
+      let darkIndexG = darkenColorArray[1];
+      let darkIndexR = darkenColorArray[2];
+
+      expect(darkIndexB).toEqual(expectedB);
+      expect(darkIndexG).toEqual(expectedG);
+      expect(darkIndexR).toEqual(expectedR);
+    });
   });
 
   describe('johndice', () => {
-
+    it('should have the same level of yellow for every set of 4 color bits', () => {
+      let expected = 0;
+      let yellowIndex = johndiceColorArray[0];
+      expect(yellowIndex).toEqual(expected);
+    });
   });
 
   describe('random', () => {
-
+    it('checking to make sure our random number is less than or equal to 255, and greater than 0', () => {
+      let lowerBound = 0;
+      let upperBound = 256;
+      let expected = getRandomNum();
+      expect(expected).toBeLessThan(upperBound);
+      expect(expected).toBeGreaterThan(lowerBound);
+    });
   });
 
   describe('redscale', () => {
